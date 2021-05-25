020fd155f4302d74d49d81f41fd94e83524e91b7
Stephanie Closson <srclosson@gmail.com>
2021-05-25 11:59:29 -0300

    fixed query not showing up in editor

--- src/Components/QueryEditor.tsx
+++ src/Components/QueryEditor.tsx
@@ -21,17 +21,19 @@ export class QueryEditor extends PureComponent<Props> {
             datasource.instanceSettings.jsonData.queryLinks?.map(
               (q: QueryLinkConfig) =>
                 ({
-                  value: q,
+                  value: q.name,
                   label: q.name,
                 } as CascaderOption)
             ) || []
           }
           onSelect={(e: string) => {
             console.log('e', e);
-            const queryLink: QueryLinkConfig = e as QueryLinkConfig;
-            console.log('selected', queryLink);
-            if (queryLink.query) {
-              onChange({ ...query, config: queryLink });
+            const queryLink: QueryLinkConfig | undefined = datasource.instanceSettings.jsonData.queryLinks?.find(
+              (ql: QueryLinkConfig) => ql.name && e === ql.name
+            );
+            if (queryLink && queryLink.query && queryLink.name) {
+              console.log('changing. Using queryLink', queryLink);
+              onChange({ ...query, name: queryLink.name, config: queryLink });
             }
           }}
         />
